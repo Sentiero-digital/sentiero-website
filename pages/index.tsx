e5b5d0665dda58644d1596d127ed426ce8377b80
Luca Pagliaro <luca@sentiero.digital>
2023-11-29 10:50:21 +0100

    feat: new hero

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,22 +36,18 @@ const Home: NextPage<{ projects: Project[] }> = ({projects = []}) => {
 
           <header className="mb-20">
             <div className="container">
-              <div className="card --no-padding py-10 px-col">
+              <div className="py-10">
                 <div className="row">
-                  <div className="col-span-12 md:col-span-8">
-                    <h1 className={'text-2xl tracking-tight mb-3 md:mb-5 font-medium text-gray-900 sm:text-5xl md:text-6xl'}>
+                  <div className="col-span-12 md:col-span-10 lg:col-span-8">
+                    <h1 className={'text-2xl tracking-tight md:mb-5 font-medium text-gray-900 sm:text-5xl md:text-6xl'}>
                       <span className="hidden">Sentiero</span>
                       <span>Creative<wbr/> web development</span>
                     </h1>
                   </div>
-                  <div className="col-span-12 md:col-start-1 md:col-span-3 2xl:col-start-1 2xl:col-span-2">
-                    <p className={'text-sm text-gray-600 md:text-lg '}>Building only beautyful products</p>
-                  </div>
-                  <div className="col-span-12 md:col-start-4 md:col-span-6">
+                  <div className="col-span-12 md:col-span-10 lg:col-span-8">
                     <p className="text-sm md:text-lg ">In love with discovering, testing and building <u>good
-                      code</u>, <wbr/>trying
-                      new technologies every day using lean approach. <wbr/>Agile and clean-code centric methodology for
-                      building maintainable, accessibility-first web solutions.
+                      code</u>, <wbr/>trying new technologies every day using lean approach. <wbr/>Agile and clean-code
+                      centric methodology for building maintainable, accessibility-first web solutions.
                     </p>
                   </div>
                 </div>
